efdf1e263a501cb3f4d5f8ff92bc101aca0ed207
Samuel <sosamaximilianosamuel@gmail.com>
2021-06-07 21:30:05 -0300

    Add variable port

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express(); // Crea un instancia de Express.
 const routes = require("./routes");
+const dotenv = require('dotenv');
+dotenv.config();
 
 routes(app);
 
-app.listen(3000, () => console.log("Servidor corriendo en el puerto 3000!"));
+app.listen(process.env.PORT || 3000, () => console.log(`Servidor corriendo en el puerto ${process.env.PORT || 3000}!`));
